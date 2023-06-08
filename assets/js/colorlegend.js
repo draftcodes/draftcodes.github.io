8f8e3fbf03055180e4d83bca4f5a3165b8ca9abc
ryan2137 <124649591+draftcodes@users.noreply.github.com>
2023-06-07 22:08:23 -0500

    Update colorlegend.js

--- assets/js/colorlegend.js
+++ assets/js/colorlegend.js
@@ -1,9 +1,9 @@
 // Define your color legend data
 const colorLegendData = [
-  { color: '#33A036', label: 'Least', class: 'shade-1-button' },
-  { color: '#147917', label: 'Lesser', class: 'shade-2-button' },
-  { color: '#023020', label: 'Greater', class: 'shade-3-button' },
-  { color: '#B59410', label: 'Greatest', class: 'shade-4-button' }
+  { color: '#33A036', label: 'Low', class: 'shade-1-button' },
+  { color: '#147917', label: 'Med', class: 'shade-2-button' },
+  { color: '#023020', label: 'High', class: 'shade-3-button' },
+  { color: '#B59410', label: 'Strong', class: 'shade-4-button' }
 ];
 
 // Create the color legend elements dynamically within the wrapper
